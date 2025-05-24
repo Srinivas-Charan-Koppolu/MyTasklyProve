aec1d3fdb2bec14fddf14c377dbdd8acca2b6b27
SrinivasCharanK <sunkush.koppolu@gmail.com>
2025-05-24 18:01:58 +0530

    Checking working of new Database

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for, flash, session
 from app import db
 from .models import User
+from config import Config
 
 def index():
     return render_template('index.html')
@@ -10,4 +11,4 @@ def about():
 
 def see_users():
     users = User.query.all()
-    return render_template('see_users.html', users=users)
+    return render_template('see_users.html', users=users, database_url=Config.SQLALCHEMY_DATABASE_URI)
--- config.py
+++ config.py
@@ -6,20 +6,27 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 INSTANCE_DIR = os.path.join(BASE_DIR, 'instance')
 DB_PATH = os.path.join(INSTANCE_DIR, 'mytasklyprove.db')
 
+os.environ.setdefault('DATABASE_URL', RENDER_DB_URL)
+
+if not os.environ.get('FLASK_ENV'):
+    os.environ['FLASK_ENV'] = 'development'
 
 # Make sure instance folder exists
-if not os.path.exists(INSTANCE_DIR):
-    os.makedirs(INSTANCE_DIR)
+# if not os.path.exists(INSTANCE_DIR):
+#     os.makedirs(INSTANCE_DIR)
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'mytasklyprove-secret')
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DB_PATH
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        RENDER_DB_URL
-    )
+    if os.environ.get('FLASK_ENV') == 'development':
+        # Use SQLite for development
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DB_PATH
+    else:
+        # Use the RENDER_DB_URL for production or deployment
+        SQLALCHEMY_DATABASE_URI = os.environ.get(
+            'DATABASE_URL',
+            RENDER_DB_URL
+        )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
